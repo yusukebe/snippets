d262cfbd800a3d846891e87421b8a9e1116f6134
Yusuke Wada <yusuke@kamawada.com>
2020-01-27 07:31:56 +0900

    Make it as submodule.

--- ruby/design-pattern/Adapter/main.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'adapter'
-
-student = HumanAdapter.new('Tanaka', 25)
-student.show_name
-student.show_age
--- ruby/design-pattern/TemplateMethod/main.rb
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-require_relative 'template_method'
-
-slime = Slime.new('スライムくん')
-dragon = Slime.new('ドラゴンさん')
-slime.show_info
-dragon.show_info
